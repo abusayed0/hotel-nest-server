e1fe1926f64daaa9cd654982c75fd9ae0080b77c
abusayed0 <abusayed48876@gmail.com>
2023-11-08 03:51:52 +0600

    created room detail and rooms booking data api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -30,9 +30,10 @@ async function run() {
 
         const hotelNestDB = client.db("hotelNestDB");
         const roomsCollection = hotelNestDB.collection("roomsCollection");
+        const bookingDateCollection = hotelNestDB.collection("bookingDateCollection");
 
-        // room page related api 
-        app.get("/rooms", async(req, res) => {
+        // rooms page related api 
+        app.get("/rooms", async (req, res) => {
             const query = {};
             const options = {
                 projection: {
@@ -41,11 +42,38 @@ async function run() {
             };
             const cursor = roomsCollection.find(query, options);
             const allRooms = await cursor.toArray();
-            
+
             res.send(allRooms)
         });
-       
 
+        // room details page related api 
+        app.get("/room/:id", async (req, res) => {
+            const roomId = req.params.id;
+
+            const query = { _id: new ObjectId(roomId) };
+
+            const room = await roomsCollection.findOne(query);
+            res.send(room);
+        });
+        app.get("/booking-data", async(req, res) => {
+            const { date, roomId } = req.query;
+            console.log(date);
+            const query = { roomId: roomId, bookedDate: date };
+            
+            // Execute query
+            const roomBookingData = await bookingDateCollection.findOne(query);
+            if(!roomBookingData){
+                res.send({
+                    status : "all room available"
+                })
+            }
+            else{
+                res.send({
+                    restSeat : roomBookingData.restSeat
+                })
+            }
+
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
